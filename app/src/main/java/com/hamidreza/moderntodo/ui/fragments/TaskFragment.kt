b07703ccf40ab07d7156476bbca47a48453bd2ad
Hamidrezakf <hrk1fard@gmail.com>
2020-12-22 12:38:17 +0330

    add menu in TaskFragment

--- app/src/main/java/com/hamidreza/moderntodo/ui/fragments/TaskFragment.kt
+++ app/src/main/java/com/hamidreza/moderntodo/ui/fragments/TaskFragment.kt
@@ -1,10 +1,9 @@
 package com.hamidreza.moderntodo.ui.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hamidreza.moderntodo.R
@@ -40,6 +39,45 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
                 layoutManager = LinearLayoutManager(requireContext())
                 setHasFixedSize(true)
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_task_menu,menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return when(item.itemId){
+
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
